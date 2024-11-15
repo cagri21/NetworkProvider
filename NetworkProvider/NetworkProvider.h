272bbecdb92350c8e413bdf82aad36d54284a4d6
Çağrı Yörükoğlu <cagriyorukoglu21@gmail.com>
2024-11-15 13:16:25 +0100

    Initial Commit

---
+++ NetworkProvider/NetworkProvider.h
@@ -0,0 +1,18 @@
+//
+//  NetworkProvider.h
+//  NetworkProvider
+//
+//  Created by Çağrı Yörükoğlu on 15.11.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkProvider.
+FOUNDATION_EXPORT double NetworkProviderVersionNumber;
+
+//! Project version string for NetworkProvider.
+FOUNDATION_EXPORT const unsigned char NetworkProviderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkProvider/PublicHeader.h>
+
+

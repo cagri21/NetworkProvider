e8bdcfc853b2036cdad72d4dbaaad6bd43df4b6e
Çağrı Yörükoğlu <cagriyorukoglu21@gmail.com>
2024-11-17 15:11:51 +0100

    Model updated with protocl

--- NetworkProvider/NetworkProvider.docc/Resources/Network/Models/Products.swift
+++ NetworkProvider/NetworkProvider.docc/Resources/Network/Models/Products.swift
@@ -7,8 +7,42 @@
 
 import Foundation
 
+public protocol ProductDisplayable {
+    var id: Int { get }
+    var title: String { get }
+    var imageURL: String { get }
+    var price: Double { get }
+    var instantDiscountPrice: Double? { get }
+}
+
+// MARK: - API Response
 public struct ProductsResponse: Codable {
-    public let page: String?
+    public let page: String
     public let nextPage: String?
-    public let published_at: String?
+    public let published_at: String
+    public let sponsoredProducts: [SponsoredProduct]?
+    public let products: [Product]
+}
+
+public struct SponsoredProduct: Codable, ProductDisplayable {
+    public let id: Int
+    public let title: String
+    public let image: String
+    public let price: Double
+    public let instantDiscountPrice: Double?
+    public let rate: Double?
+
+    public var imageURL: String { image }
+}
+
+public struct Product: Codable, ProductDisplayable {
+    public let id: Int
+    public let title: String
+    public let image: String
+    public let price: Double
+    public let instantDiscountPrice: Double?
+    public let rate: Double?
+    public let sellerName: String?
+
+    public var imageURL: String { image }
 }

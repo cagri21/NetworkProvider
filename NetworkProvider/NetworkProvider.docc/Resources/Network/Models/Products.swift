33194bead33697001ff62a88e3380110dee00cdb
Çağrı Yörükoğlu <cagriyorukoglu21@gmail.com>
2024-11-19 18:59:50 +0100

    Model optionals updated.

--- NetworkProvider/NetworkProvider.docc/Resources/Network/Models/Products.swift
+++ NetworkProvider/NetworkProvider.docc/Resources/Network/Models/Products.swift
@@ -15,8 +15,8 @@ public protocol ProductDisplayable: Codable {
     var title: String { get }
     var image: String { get }
     var price: Double { get }
-    var instantDiscountPrice: Double { get }
-    var rate: Double { get }
+    var instantDiscountPrice: Double? { get }
+    var rate: Double? { get }
     var imageData: UIImage? { get set }
 
     func getImage(completion: @escaping (UIImage?) -> Void)
@@ -46,12 +46,21 @@ public struct ProductsResponse: Codable {
     public let sponsoredProducts: [SponsoredProduct]?
     public let products: [Product]
 
-    enum CodingKeys: String, CodingKey {
+    private enum CodingKeys: String, CodingKey {
         case page
         case nextPage
         case publishedAt = "published_at"
         case sponsoredProducts
         case products
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        page = try container.decode(String.self, forKey: .page)
+        nextPage = try container.decodeIfPresent(String.self, forKey: .nextPage)
+        publishedAt = try container.decode(String.self, forKey: .publishedAt)
+        sponsoredProducts = try container.decodeIfPresent([SponsoredProduct].self, forKey: .sponsoredProducts)
+        products = try container.decode([Product].self, forKey: .products)
     }
 }
 
@@ -61,13 +70,23 @@ public struct SponsoredProduct: ProductDisplayable {
     public var title: String
     public var image: String
     public var price: Double
-    public var instantDiscountPrice: Double
-    public var rate: Double
+    public var instantDiscountPrice: Double?
+    public var rate: Double?
     public var imageData: UIImage? = nil // Exclude this from decoding
 
-    // Define CodingKeys to exclude `imageData` from decoding
     private enum CodingKeys: String, CodingKey {
         case id, title, image, price, instantDiscountPrice, rate
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        title = try container.decode(String.self, forKey: .title)
+        image = try container.decode(String.self, forKey: .image)
+        price = try container.decode(Double.self, forKey: .price)
+        instantDiscountPrice = try container.decodeIfPresent(Double.self, forKey: .instantDiscountPrice)
+        rate = try container.decodeIfPresent(Double.self, forKey: .rate)
+        imageData = nil // Explicitly set as nil since it shouldn't be decoded
     }
 }
 
@@ -77,13 +96,24 @@ public struct Product: ProductDisplayable {
     public var title: String
     public var image: String
     public var price: Double
-    public var instantDiscountPrice: Double
-    public var rate: Double
+    public var instantDiscountPrice: Double?
+    public var rate: Double?
     public var sellerName: String
     public var imageData: UIImage? = nil // Exclude this from decoding
 
-    // Define CodingKeys to exclude `imageData` from decoding
     private enum CodingKeys: String, CodingKey {
         case id, title, image, price, instantDiscountPrice, rate, sellerName
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        title = try container.decode(String.self, forKey: .title)
+        image = try container.decode(String.self, forKey: .image)
+        price = try container.decode(Double.self, forKey: .price)
+        instantDiscountPrice = try container.decodeIfPresent(Double.self, forKey: .instantDiscountPrice)
+        rate = try container.decodeIfPresent(Double.self, forKey: .rate)
+        sellerName = try container.decode(String.self, forKey: .sellerName)
+        imageData = nil // Explicitly set as nil since it shouldn't be decoded
     }
 }
